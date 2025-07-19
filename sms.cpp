10e85bfa6ff230e0b0f894e3802592207e51c244
SalehAskari <salehaskari.4992@gmail.com>
2025-07-19 17:39:28 +0430

    created structure.

---
+++ sms.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+enum State{
+  CLIANT,
+  SERVER
+};
+class Server {
+  public:
+
+  private:
+};
+
+class Cliant {
+  public:
+
+  private:
+};
+
+
+class Interface{
+  public:
+
+  private:
+};
+
+int main (int argc, char *argv[]) {
+  
+  return 0;
+}
